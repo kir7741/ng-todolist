dbd124679ccb79bee71f1afaf34189521f07c5d0
kir7741 <typhoonjoe2003@yahoo.com.tw>
2018-07-11 21:22:06 +0800

    新增需要的頁面

--- src/app/complete/complete.module.ts
+++ src/app/complete/complete.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CompleteComponent } from './complete.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [CompleteComponent]
 })
 export class CompleteModule { }
